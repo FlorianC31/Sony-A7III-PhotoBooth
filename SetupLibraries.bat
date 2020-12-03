bbc91d0cc5550f58f64212cd9a886aab889ff459
FlorianC31 <f.champain@gmail.com>
2020-12-03 10:39:03 +0100

    Correction du bat d'installation des librairies

--- SetupLibraries.bat
+++ SetupLibraries.bat
@@ -1,10 +1,10 @@
 pip install --upgrade pip
 pip install --upgrade setuptools
+pip install wheel
 pip install opencv-python
 pip install PyQt5
 pip install keyboard
 pip install pywin32
 pip install pywinauto
-python pywinauto-0.6.8\setup.py install --user
 pip install ftd2xx
 pip install Pillow-PIL
